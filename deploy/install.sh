89200f78c3c1bb5f1f44203e07ce93e01bdfe54c
Antony Lee <antony.lee@qsan.com>
2023-05-21 23:52:51 -0400

    Release 1.0.0

---
+++ deploy/install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+BASE_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+if kubectl get node --show-labels | grep -q topology.qsan.com/fc; then
+	kubectl label nodes --all topology.qsan.com/fc-
+fi
+
+kubectl create ns qsan
+kubectl create secret -n qsan generic qsan-auth-secret --from-file=${BASE_DIR}/qsan-auth.yaml
+
+kubectl apply -f ${BASE_DIR}/rbac.yaml
+kubectl apply -f ${BASE_DIR}/driverinfo.yaml
+kubectl apply -f ${BASE_DIR}/controller.yaml
+kubectl apply -f ${BASE_DIR}/node.yaml
+
---
+++ deploy/uninstall.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+BASE_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+kubectl delete -f ${BASE_DIR}/node.yaml
+kubectl delete -f ${BASE_DIR}/controller.yaml
+kubectl delete -f ${BASE_DIR}/driverinfo.yaml
+kubectl delete -f ${BASE_DIR}/rbac.yaml
+kubectl delete -f ${BASE_DIR}/secret.yaml
+
+#kubectl delete secret qsan-auth-secret -n qsan
+#kubectl delete ns qsan
